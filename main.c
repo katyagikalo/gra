bde96eb275fb59c3c83adb668ce59eb3e775ecc4
katya <61763880+katyagikalo@users.noreply.github.com>
2021-12-04 13:54:25 +0100

    Update main.c

--- main.c
+++ main.c
@@ -31,21 +31,22 @@ double regula_falsi(double(* fn)(double), double a, double b, unsigned n){
 int main(void){
     double a = -2, b = 4;
     
-    double c_result = regula_falsi(fn, a, b, 6);
+    /*double c_result = regula_falsi(fn, a, b, 6);
     double ass_result = regulafalsi(fn, a, b, 6);
    
     c_result = regula_falsi(fn, a, b, 7);
-    ass_result = regulafalsi(fn, a, b, 7);
+    ass_result = regulafalsi(fn, a, b, 7);*/
     
-    /*for(int i = 6; i < 8; i++){
-        double c_result = regula_falsi(fn, a, b, i);
-        double ass_result = regulafalsi(fn, a, b, i);
+    for(int i = 6; i < 8; i++){
+        double c_result = regula_falsi(fn, a, b, 6);
+        double ass_result = regulafalsi(fn, a, b, 6);
         
         if(c_result != ass_result){
             printf("results don't match! a is: %f, b is: %f, n is: %d\n", a, b, i);
             printf("c result was: %f\n", c_result);
             printf("assembler result was: %f\n", ass_result);
         }
-    }*/
+    }
+    
     return 0;
 }
