313f9e08cfa73b9e88987a09e31ad4535171b97a
katya <61763880+katyagikalo@users.noreply.github.com>
2021-12-04 14:01:38 +0100

    Update main.c

--- main.c
+++ main.c
@@ -28,26 +28,42 @@ double regula_falsi(double(* fn)(double), double a, double b, unsigned n){
     }
 }
 
-int main(void){
-    double a = -2, b = 4;
-    
-    /*double c_result = regula_falsi(fn, a, b, 6);
-    double ass_result = regulafalsi(fn, a, b, 6);
-   
-    c_result = regula_falsi(fn, a, b, 7);
-    ass_result = regulafalsi(fn, a, b, 7);*/
-    double c_result, ass_result;
-    
-    for(int i = 6; i < 8; i++){
-        c_result = regula_falsi(&fn, a, b, 6);
-        ass_result = regulafalsi(&fn, a, b, 6);
-        
-        /*if(c_result != ass_result){
-            printf("results don't match! a is: %f, b is: %f, n is: %d\n", a, b, i);
+void check(double a, double b, unsigned n, double c_result, double ass_result){
+    if(c_result != ass_result){
+            printf("results don't match! a is: %f, b is: %f, n is: %d\n", a, b, n);
             printf("c result was: %f\n", c_result);
             printf("assembler result was: %f\n", ass_result);
-        }*/
     }
+}
+
+int main(void){
+    double a = -2, b = 4;
+    unsigned n = 0;
+    
+    double c_result = regula_falsi(fn, a, b, n);
+    double ass_result = regulafalsi(fn, a, b, n);
+    check(a, b, n, c_result, ass_result);
+   
+    c_result = regula_falsi(fn, a, b, n+1);
+    ass_result = regulafalsi(fn, a, b, n+1);
+    check(a, b, n+1, c_result, ass_result);
+    
+    c_result = regula_falsi(fn, a, b, n+2);
+    ass_result = regulafalsi(fn, a, b, n+2);
+    check(a, b, n+2, c_result, ass_result);
+    
+    c_result = regula_falsi(fn, a, b, n+3);
+    ass_result = regulafalsi(fn, a, b, n+3);
+    check(a, b, n+3, c_result, ass_result);
+    
+    c_result = regula_falsi(fn, a, b, n+4);
+    ass_result = regulafalsi(fn, a, b, n+4);
+    check(a, b, n+4, c_result, ass_result);
+    
+    /*for(int i = 6; i < 8; i++){
+        c_result = regula_falsi(&fn, a, b, i);
+        ass_result = regulafalsi(&fn, a, b, i);
+    }*/
     
     return 0;
 }
